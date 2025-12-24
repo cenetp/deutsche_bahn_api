566c9e2cc1b531144303cf81e8fc72a1dab3006b
Viktor Eisenstadt <vsa85@gmx.de>
2025-12-24 02:11:26 +0100

    remove test

--- test.py
+++
@@ -1,7 +0,0 @@
-import deutsche_bahn_api.api_authentication as dba
-import deutsche_bahn_api.timetable_helper as dbt
-import deutsche_bahn_api.station_helper as dbc_s
-
-api = dba.ApiAuthentication("1ac3d18ab81777747efe5f41b6b78ca5", "076006a97b68f3fd1c914ede788c443f")
-station_helper = dbc_s.StationHelper()
-print(station_helper.find_stations_by_lat_long(48.209166953908536, 11.34949095056929, 2))
